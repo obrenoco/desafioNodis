0f411fa447d9153074762dbaf738419efd0bcbd0
Your Name <bsromeiro@gmail.com>
2020-11-05 11:24:34 -0300

    config | add: routing system

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Text} from 'react-native';
+
+import {Container} from './styles';
+
+const Details: React.FC = () => {
+  return (
+    <Container>
+      <Text>Details</Text>
+    </Container>
+  );
+};
+
+export default Details;
---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreen from './pages/Home';
+import DetailsScreen from './pages/Details';
+
+const Routes = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {title: 'Home'},
+    },
+    Details: {
+      screen: DetailsScreen,
+      navigationOptions: {title: 'Details'},
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+export default Routes;
