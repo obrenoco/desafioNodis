762c8fd45e39302e370a2742f80f410836610586
Your Name <bsromeiro@gmail.com>
2020-11-07 21:57:05 -0300

    add: logo on header

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,34 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeScreen from './pages/Home';
 import DetailsScreen from './pages/Details';
+import {Image} from 'react-native';
 
 const Stack = createStackNavigator();
+
+function LogoTitle() {
+  return (
+    <Image
+      style={{
+        width: 50,
+        height: 50,
+      }}
+      source={require('../assets/images/logo.png')}
+    />
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerTitle: () => <LogoTitle />,
+            headerTitleAlign: 'center',
+          }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
