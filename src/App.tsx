9e6e877490bb860aa907084b92e201f58bd1c484
Your Name <bsromeiro@gmail.com>
2020-11-21 14:11:17 -0300

    update: status bar style

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import * as React from 'react';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 
 import Route from './navigation/routes';
+import {StatusBar} from 'react-native';
 
 const client = new ApolloClient({
   //uri: 'http://localhost:3000', <= Para emuladores iOS
@@ -16,6 +17,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <StatusBar barStyle="dark-content" backgroundColor={'#fff'} />
       <Route />
     </ApolloProvider>
   );
