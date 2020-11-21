af3bc27041f6a40a1ce5719c41677bc606407d9f
Your Name <bsromeiro@gmail.com>
2020-11-21 14:48:26 -0300

    update: input position

--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -50,7 +50,7 @@ export const StockInput = styled.TextInput`
   height: 36px;
   width: 92px;
   border-radius: 3px;
-  padding-left: 13px;
+  padding: 0 13px;
 `;
 
 export const StockButton = styled.TouchableOpacity`
@@ -85,8 +85,10 @@ export const PriceInput = styled.TextInput`
   border-color: #c4cdd5;
   border-width: 1px;
   width: 100%;
-  padding-left: 13px;
+  padding: 0 13px;
   height: 36px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Measures = styled.View`
